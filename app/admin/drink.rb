0631831277fdd030418c924efc60a833e9de4d3e
bennypaulino <santiagopro71@gmail.com>
2017-03-19 22:29:23 -0400

    Adjust indentation

--- app/admin/drink.rb
+++ app/admin/drink.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Drink do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :title, :description, :steps, :source
+  permit_params :title, :description, :steps, :source
 #
 # or
 #
